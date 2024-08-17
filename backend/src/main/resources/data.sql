87d266bed32adeb96d6b9158bf87d1d173ccc516
Gabriel Gomes <gabrielbdf@hotmail.com>
2024-08-17 13:29:20 -0300

    ajustes no spring

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 
 
 TRUNCATE TABLE operation RESTART IDENTITY CASCADE;
-TRUNCATE TABLE users RESTART IDENTITY CASCADE;
+TRUNCATE TABLE users RESTART IDENTITY CASCADE; 
 INSERT INTO operation (cost, operation, groovy_expression) VALUES (10, 'ADD', 'args.collect{it.toLong()}.sum().toString()');
 INSERT INTO operation (cost, operation, groovy_expression) VALUES (20, 'SUBTRACT', 'String.valueOf(args.collect { it.toLong() }.inject { a, b -> a - b } ?: 0)');
 INSERT INTO operation (cost, operation, groovy_expression) VALUES (30, 'MULTIPLY', 'String.valueOf(args.collect { it.toLong() }.inject { a, b -> a * b } ?: 0)');
